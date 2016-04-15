e7d3e7d44a093a6c396b80f52a050a5e463cbae1
Jeffrey <jeffreydvp@gmail.com>
2016-04-15 09:20:22 -0600

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -32,7 +32,7 @@ function checksrv {
     df -h
     df -i
     sarall -q|egrep '[1-9]\.'|grep -v Linux
-    sarall -r|awk '{print $1" "$*}'|egrep '([8-9][1-9]|[1-9][0-9][0-9])\.'|grep -v Linux
+    sarall -r|awk '{print $1" "$8}'|egrep '([8-9][1-9]|[1-9][0-9][0-9])\.'|grep -v Linux
 }
 function wpcli {
     wget -q -O ~/tmp/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar

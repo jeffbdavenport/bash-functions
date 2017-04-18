ae62c68e6fbff9f550e56eec002157c156176b85
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-04-18 00:08:07 -0600

    Added help

--- main.sh
+++ main.sh
@@ -8,9 +8,9 @@ gitc() {
     gitc_help
     return 0
   fi
+  echo 'test'
   # Don't push to these branches
   declare -A safe_branches=([develop]=1 [dev]=1 [master]=1)
-  echo 'test'
   if [ "$1" == '-u' ] || [ "$1" == '-A' ];then
     git add "$1"
     commit="${@:2}"

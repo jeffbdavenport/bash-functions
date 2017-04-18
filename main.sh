2ccad5b3fefe151c535a90df2efe2ea2442b7155
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-04-17 23:33:40 -0600

    gitc function

--- main.sh
+++ main.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -l
-function base(){
+myenv() { . ~/my_projects/bash-functions/main.sh; }
+
+gitc() {
+  if [ "$1" == '-u' ] || [ "$1" == '-A' ];then
+    git add "$1"
+  fi
+  if ! $(git diff-index --quiet HEAD --);then
+    git commit -m "${@:2}"
+    if [ "$(git rev-parse --abbrev-ref HEAD)" == 'rails-base' ];then
+      git push -u rails-base rails-base:master
+    else
+      git push
+    fi
+  fi
 }

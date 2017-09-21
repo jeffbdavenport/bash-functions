95f99f226cf98190646d021ea7c755010a81a488
Jeffrey Davenport <jeffrey.davenport@chargeback.com>
2017-09-21 10:58:27 -0600

    Add ext

--- main.sh
+++ main.sh
@@ -3,12 +3,13 @@ bash_root=~/my_projects/bash-functions
 alias compare_develop='branch=$(git branch |grep "*"|sed "s/^* //");git checkout -b compare_develop && git merge develop --squash && git commit -m "merge"; git diff develop && git checkout "$branch" && git branch -D compare_develop'
 gitenv() { . <(curl -sS https://raw.githubusercontent.com/jeffreydvp/bash-functions/master/functions.sh); }
 myenv() { . $bash_root/main.sh; }
-
+xmod() { xmodmap ~/.Xmodmap; }
 if [ "$0" != 'bash' ];then
   bash -l
   return 0
 fi
-
+. ~/git_prompt.bash
+PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
 clean_branches(){
   git checkout develop
   for b in `git branch --merged|sed "/^\*/d"|sed "/develop/d"`;do
@@ -83,6 +84,29 @@ function duse(){
   du -axh "$dir"|sort -hr|head -n "$lines"|sort -k 2,2|sed "s/^\([0-9]*\)\([MKG]\)/\1.0\2/;s/^\([0-9]\.[0-9][MK]\)/0\1/"
 }
 
+function ext()
+# Handy Extract Program
+{
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1     ;;
+            *.tar.gz)    tar xvzf $1     ;;
+            *.bz2)       bunzip2 $1      ;;
+            *.rar)       unrar x $1      ;;
+            *.gz)        gunzip $1       ;;
+            *.tar)       tar xvf $1      ;;
+            *.tbz2)      tar xvjf $1     ;;
+            *.xz)        tar xvJf $1     ;;
+            *.tgz)       tar xvzf $1     ;;
+            *.zip)       unzip $1        ;;
+            *.Z)         uncompress $1   ;;
+            *.7z)        7z x $1         ;;
+            *)           echo "'$1' cannot be extracted via >extract<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
 
 
 
